e2ddae79aa3545d10a28e184728a061828af15e8
Le Ton Chanh <letonchanh@gmail.com>
2016-09-02 18:23:15 +0800

    my spacemacs config

---
+++ private/lech-init.el
@@ -0,0 +1,126 @@
+(defun lech/init-configs ()
+  ;; general
+  (setq global-linum-mode t)
+
+  ;; shell
+  (setq comint-prompt-read-only nil)
+  (setq comint-scroll-to-bottom-on-input t)
+
+  ;; boogie-friends
+  ;; (setq flycheck-dafny-executable "PATH-TO-Dafny.exe")
+  ;; (setq flycheck-boogie-executable "PATH-TO-BOOGIE")
+  ;; (setq flycheck-z3-smt2-executable "/usr/local/bin/z3")
+  ;; (setq flycheck-inferior-dafny-executable "PATH-TO-DafnyServer.exe") ;; Optional
+  ;; (setq boogie-friends-profile-analyzer-executable "PATH-TO-Z3-AXIOM-PROFILER") ;; Optional
+)
+
+(defun lech/init-keys ()
+  ;; change buffer to helm-mini
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-h v") 'customize-variable-other-window)
+
+  ;; change comment-dwim to comment-dwim-2
+  (global-set-key (kbd "M-;") 'comment-dwim-2)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; INIT SPACELINE
+
+(require 'spaceline-segments)
+
+;; reuse code from spaceline-config.el
+(defun tddsg--create-spaceline-theme (left second-left &rest additional-segments)
+  "Convenience function for the spacemacs and emacs themes."
+  (require 'latex-preview-pane)
+  (spaceline-install 'tddsg
+                     `(,left
+                       anzu
+                       auto-compile
+                       ,second-left
+                       major-mode
+                       (process :when active)
+                       ((flycheck-error flycheck-warning flycheck-info)
+                        :when active)
+                       (minor-modes :when active)
+                       (mu4e-alert-segment :when active)
+                       (erc-track :when active)
+                       (version-control :when active)
+                       (org-pomodoro :when active)
+                       (org-clock :when active)
+                       nyan-cat)
+                     `(which-function
+                       (python-pyvenv :fallback python-pyenv)
+                       (battery :when active)
+                       selection-info
+                       input-method
+                       ((buffer-encoding-abbrev))
+                       (global :when active)
+                       ,@additional-segments
+                       buffer-position
+                       hud))
+
+  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-tddsg)))))
+
+;;; used for setting pdf-view page
+;;; FIXME: to be removed when spaceline is updated
+(declare-function pdf-view-current-page 'pdf-view)
+(declare-function pdf-cache-number-of-pages 'pdf-view)
+
+(defun tddsg--pdfview-page-number ()
+  (format "(%d/%d)"
+          (eval (pdf-view-current-page))
+          (pdf-cache-number-of-pages)))
+
+(spaceline-define-segment line-column
+  "The current line and column numbers, or `(current page/number of pages)`
+in pdf-view mode (enabled by the `pdf-tools' package)."
+  (if (eq 'pdf-view-mode major-mode)
+      (tddsg--pdfview-page-number)
+    "%l:%2c"))
+
+
+(defun tddsg--create-spaceline-final (&rest additional-segments)
+  "Install the modeline used by Spacemacs.
+ADDITIONAL-SEGMENTS are inserted on the right, between `global' and
+`buffer-position'."
+  (require 'spaceline)
+  (require 'pdf-view)
+  (apply 'tddsg--create-spaceline-theme
+         '((persp-name
+            workspace-number
+            window-number)
+           :fallback evil-state
+           :separator "|"
+           :face highlight-face)
+         '(buffer-modified
+           point-position
+           line-column
+           ;; buffer-size
+           buffer-id
+           remote-host)
+         additional-segments))
+
+(dolist (s '((tddsg-face-unmodified "SteelBlue3" "Unmodified buffer face.")
+             (tddsg-face-modified "DarkGoldenrod2" "Modified buffer face.")
+             (tddsg-face-read-only "SteelBlue3" "Read-only buffer face.")))
+  (eval `(defface, (nth 0 s)
+           `((t (:background ,(nth 1 s)
+                             :foreground "#3E3D31"
+                             :inherit 'mode-line)))
+           ,(nth 2 s)
+           :group 'tddsg)))
+
+
+(defun tddsg--spaceline-highlight-face ()
+  "Set the highlight face depending on the buffer modified status.
+Set `spaceline-highlight-face-func' to
+`tddsg--spaceline-highlight-face' to use this."
+  (cond
+   (buffer-read-only 'tddsg-face-read-only)
+   ((buffer-modified-p) 'tddsg-face-modified )
+   (t 'tddsg-face-unmodified)))
+
+
+(defun lech/init-spaceline ()
+  (setq spaceline-highlight-face-func 'tddsg--spaceline-highlight-face)
+  (tddsg--create-spaceline-final))
---
+++ private/lech/packages.el
@@ -0,0 +1,106 @@
+;;; packages.el --- lech layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Le Ton Chanh <chanhle@mint-chanhle>
+;; URL: https://github.com/letonchanh
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `lech-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `lech/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `lech/pre-init-PACKAGE' and/or
+;;   `lech/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst lech-packages
+  '(;; Packages for emacs display
+    helm
+    ;; Packages for editor
+    comment-dwim-2
+    ;; Packages for LaTeX
+    auctex
+    ; latex-extra
+    pdf-tools
+    latex-preview-pane
+    ;; Package for z3
+    boogie-friends)
+
+  "The list of Lisp packages required by the lech layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+
+;;; INIT PACKAGES
+
+(defun lech/init-comment-dwim-2 ()
+  (require 'comment-dwim-2))
+
+(defun lech/init-pdf-tools ()
+  (require 'pdf-tools)
+  (pdf-tools-install))
+
+(defun lech/init-latex-preview-pane ()
+  (require 'latex-preview-pane))
+
+(defun lech/post-init-auctex ()
+  (require 'tex)
+  ;; (add-to-list 'TeX-command-list '("Make" "make" TeX-run-compile nil t))
+  (custom-set-variables
+   '(TeX-save-query nil)
+   '(TeX-source-correlate-method (quote synctex))
+   '(TeX-source-correlate-mode t)
+   '(TeX-source-correlate-start-server t)
+   '(LaTeX-command "latex --synctex=1")
+   '(TeX-view-program-list
+     (quote (("pdf-tools" "TeX-pdf-tools-sync-view"))))
+   '(TeX-view-program-selection
+     (quote ((engine-omega "dvips and gv")
+             (output-dvi "xdvi")
+             (output-pdf "pdf-tools")
+             (output-html "xdg-open"))))))
+
+(defun lech/init-boogie-friends ()
+  (require 'boogie-friends)
+  (setq flycheck-z3-smt2-executable "/usr/local/bin/z3"))
+
+;;; packages.el ends here
